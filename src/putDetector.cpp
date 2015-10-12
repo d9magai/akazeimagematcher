d182b5a4febaea5c1a38abdabb4ac00de37339d8
d9magai <kumagai@sencorp.co.jp>
2015-10-12 22:11:13 +0900

    include matSerialization

--- src/putDetector.cpp
+++ src/putDetector.cpp
@@ -9,49 +9,10 @@
 #include <aws/s3/model/PutObjectRequest.h>
 #include <aws/core/utils/HashingUtils.h>
 #include <aws/core/utils/StringUtils.h>
-#include <boost/shared_ptr.hpp>
-#include <boost/archive/binary_oarchive.hpp>
-#include <boost/serialization/vector.hpp>
+#include "matSerialization.h"
 
 const Aws::String AWS_ACCESS_KEY_ID = std::getenv("AWS_ACCESS_KEY_ID");
 const Aws::String AWS_SECRET_ACCESS_KEY = std::getenv("AWS_SECRET_ACCESS_KEY");
-
-BOOST_SERIALIZATION_SPLIT_FREE(cv::Mat)
-namespace boost {
-namespace serialization {
-
-/*** Mat ***/
-template<class Archive>
-void save(Archive & ar, const cv::Mat& m, const unsigned int version) {
-    size_t elemSize = m.elemSize(), elemType = m.type();
-    ar & m.cols;
-    ar & m.rows;
-    ar & elemSize;
-    ar & elemType; // element type.
-    size_t dataSize = m.cols * m.rows * m.elemSize();
-    for (size_t dc = 0; dc < dataSize; ++dc) {
-        ar & m.data[dc];
-    }
-}
-
-template<class Archive>
-void load(Archive & ar, cv::Mat& m, const unsigned int version) {
-    int cols, rows;
-    size_t elemSize, elemType;
-
-    ar & cols;
-    ar & rows;
-    ar & elemSize;
-    ar & elemType;
-
-    m.create(rows, cols, elemType);
-    size_t dataSize = m.cols * m.rows * elemSize;
-    for (size_t dc = 0; dc < dataSize; ++dc) {
-        ar & m.data[dc];
-    }
-}
-}
-}
 
 cv::Mat getImage(Aws::S3::S3Client s3Client, Aws::String key) {
 
