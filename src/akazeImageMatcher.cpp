638560771ad25658cfc4b29f8ca4a25522a04b35
d9magai <kumagai@sencorp.co.jp>
2015-10-12 23:13:57 +0900

    use getDescriptor in akazeImageMatcher

--- src/akazeImageMatcher.cpp
+++ src/akazeImageMatcher.cpp
@@ -1,11 +1,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/features2d.hpp>
-#include <aws/core/utils/StringUtils.h>
-#include <aws/s3/model/GetObjectRequest.h>
 #include "matSerialization.h"
 #include "commons.h"
 #include "s3Utils.h"
@@ -17,35 +13,19 @@ int main(int argc, char** argv) {
 
     try {
         cv::Ptr<cv::FeatureDetector> d = cv::AKAZE::create();
-        cv::Ptr<cv::flann::IndexParams> indexParams = new cv::flann::LshIndexParams(12, 20, 2);
-        cv::Ptr<cv::FlannBasedMatcher> m = cv::Ptr<cv::FlannBasedMatcher>(new cv::FlannBasedMatcher(indexParams));
-
-        std::vector<cv::Mat> desc;
-        Aws::S3::Model::GetObjectRequest getObjectRequest;
-        getObjectRequest.SetBucket("d9magai.mybucket");
-        getObjectRequest.SetKey("path/to/matcher1");
-        auto getObjectOutcome = s3client.GetObject(getObjectRequest);
-        if (!getObjectOutcome.IsSuccess()) {
-            std::stringstream ss;
-            ss << "File download failed from s3 with error " << getObjectOutcome.GetError().GetMessage() << std::endl;
-            throw ss.str();
-        }
-        std::stringstream ss;
-        ss << getObjectOutcome.GetResult().GetBody().rdbuf();
-        boost::archive::binary_iarchive ar(ss);
-        ar >> desc;
-        cv::Ptr<cv::FlannBasedMatcher> m2 = cv::Ptr<cv::FlannBasedMatcher>(new cv::FlannBasedMatcher(indexParams));
-        m2->add(desc);
-        m2->train();
-
-        cv::Mat image = d9magai::s3utils::getImage(s3client, d9magai::commons::BUCKET, "path/to/img.jpg");
         std::vector<cv::KeyPoint> kp;
+        cv::Mat image = d9magai::s3utils::getImage(s3client, d9magai::commons::BUCKET, "path/to/img.jpg");
         d->detect(image, kp);
         cv::Mat queryDesc;
         d->compute(image, kp, queryDesc);
 
+        cv::Ptr<cv::flann::IndexParams> indexParams = new cv::flann::LshIndexParams(12, 20, 2);
+        cv::Ptr<cv::FlannBasedMatcher> m = cv::Ptr<cv::FlannBasedMatcher>(new cv::FlannBasedMatcher(indexParams));
+        std::vector<cv::Mat> desc = d9magai::s3utils::getDescriptor(s3client, d9magai::commons::BUCKET, "path/to/matcher1");
         std::vector<cv::DMatch> matches;
-        m2->match(queryDesc, matches);
+        m->add(desc);
+        m->train();
+        m->match(queryDesc, matches);
 
         int length = s.size();
         int votes[length]; // 学習画像の投票箱
@@ -67,7 +47,7 @@ int main(int argc, char** argv) {
                 maxVotes = votes[i]; //マッチした特徴点の数
             }
         }
-        std::vector<cv::Mat> trainDescs = m2->getTrainDescriptors();
+        std::vector<cv::Mat> trainDescs = m->getTrainDescriptors();
         float similarity = (float) maxVotes / trainDescs[maxImageId].rows * 100;
         if (similarity < 5) {
             maxImageId = -1; // マッチした特徴点の数が全体の5%より少なければ、未検出とする
