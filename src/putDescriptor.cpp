577ecffd0c7e506a19f191d90f2f1c39b75ab067
d9magai <kumagai@sencorp.co.jp>
2015-10-12 22:58:34 +0900

    use putDescriptor in putDescriptor.cpp

--- src/putDescriptor.cpp
+++ src/putDescriptor.cpp
@@ -2,9 +2,6 @@
 #include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
-#include <aws/s3/model/GetObjectRequest.h>
-#include <aws/s3/model/PutObjectRequest.h>
-#include <aws/core/utils/HashingUtils.h>
 #include "matSerialization.h"
 #include "commons.h"
 #include "s3Utils.h"
@@ -15,49 +12,24 @@ int main(int argc, char** argv) {
 
     try {
         cv::Ptr<cv::FeatureDetector> d = cv::AKAZE::create();
-        cv::Ptr<cv::flann::IndexParams> indexParams = new cv::flann::LshIndexParams(12, 20, 2);
-        cv::Ptr<cv::FlannBasedMatcher> m = cv::Ptr<cv::FlannBasedMatcher>(new cv::FlannBasedMatcher(indexParams));
 
         std::vector<Aws::String> s = { "path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png" };
         int i = 0;
-        std::vector<cv::Mat> addDesc;
+        std::vector<cv::Mat> descriptors;
         for (auto itr = s.begin(); itr != s.end(); ++itr) {
             std::cout << i << ":" << (*itr) << std::endl;
             cv::Mat image = d9magai::s3utils::getImageFromS3(s3clinet, d9magai::commons::BUCKET, (*itr));
             std::vector<cv::KeyPoint> kp;
             d->detect(image, kp);
             std::cout << "detected: " << kp.size() << std::endl;
-            cv::Mat desc;
-            d->compute(image, kp, desc);
-            std::cout << "descriptor size: " << desc.size() << std::endl;
-            addDesc.push_back(desc);
+            cv::Mat descriptor;
+            d->compute(image, kp, descriptor);
+            std::cout << "descriptor size: " << descriptor.size() << std::endl;
+            descriptors.push_back(descriptor);
             i++;
         }
-        m->add(addDesc);
-        m->train();
 
-        std::string result;
-        std::stringstream ss;
-        boost::archive::binary_oarchive oa(ss);
-        oa << addDesc;
-        result = ss.str();
-
-        Aws::S3::Model::PutObjectRequest putObjectRequest;
-        putObjectRequest.SetBucket("d9magai.mybucket");
-        std::shared_ptr<Aws::IOStream> objectStream = Aws::MakeShared<Aws::StringStream>("PutDetector");
-        *objectStream << result;
-        objectStream->flush();
-        putObjectRequest.SetBody(objectStream);
-        putObjectRequest.SetContentLength(static_cast<long>(putObjectRequest.GetBody()->tellp()));
-        putObjectRequest.SetContentMD5(Aws::Utils::HashingUtils::Base64Encode(Aws::Utils::HashingUtils::CalculateMD5(*putObjectRequest.GetBody())));
-        putObjectRequest.SetContentType("application/octet-stream");
-        putObjectRequest.SetKey("path/to/matcher1");
-        Aws::S3::Model::PutObjectOutcome putObjectOutcome = s3clinet.PutObject(putObjectRequest);
-        if (!putObjectOutcome.IsSuccess()) {
-            std::stringstream ss;
-            ss << "Object put failed tos3 with error :" << putObjectOutcome.GetError().GetMessage() << std::endl;
-            throw ss.str();
-        }
+        d9magai::s3utils::putDescriptor(s3clinet, descriptors, d9magai::commons::BUCKET, "path/to/matcher1");
 
     } catch (std::string str) {
         std::cerr << str << std::endl;
