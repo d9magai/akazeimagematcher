c01765bdeb97fc4dd0307bcbaf4af0e4e267fdc8
d9magai <kumagai@sencorp.co.jp>
2015-10-12 22:13:41 +0900

    add commons

---
+++ src/commons.h
@@ -0,0 +1,17 @@
+/*
+ * commons.h
+ *
+ *  Created on: 2015/10/12
+ *      Author: d9magai
+ */
+
+#ifndef COMMONS_H_
+#define COMMONS_H_
+
+#include <aws/core/utils/StringUtils.h>
+
+const Aws::String AWS_ACCESS_KEY_ID = std::getenv("AWS_ACCESS_KEY_ID");
+const Aws::String AWS_SECRET_ACCESS_KEY = std::getenv("AWS_SECRET_ACCESS_KEY");
+const Aws::String BUCKET = "d9magai.mybudket";
+
+#endif /* COMMONS_H_ */
