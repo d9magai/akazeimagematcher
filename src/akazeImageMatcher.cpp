3244778b7b01822c92673200f67934d39f7f6ea7
d9magai <kumagai@sencorp.co.jp>
2015-10-15 15:35:53 +0900

    sokutei

--- src/akazeImageMatcher.cpp
+++ src/akazeImageMatcher.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include <cstdlib>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
 #include "matSerialization.h"
@@ -8,25 +9,54 @@
 
 int main(int argc, char** argv) {
 
+    auto start = std::chrono::system_clock::now();
+
     Aws::S3::S3Client s3client = d9magai::s3utils::getS3client();
-    std::vector<Aws::String> s = { "path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png" };
+    std::vector<Aws::String> s = { "path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" ,"path/to/f_lena.jpg", "path/to/img.jpg", "path/to/graf1.png", "path/to/graf3.png", "path/to/rotate.jpg" };
 
     try {
         cv::Ptr<cv::FeatureDetector> d = cv::AKAZE::create();
         std::vector<cv::KeyPoint> kp;
-        cv::Mat image = d9magai::s3utils::getImage(s3client, d9magai::commons::BUCKET, "path/to/img.jpg");
+        cv::Mat image = d9magai::s3utils::getImage(s3client, d9magai::commons::BUCKET, "path/to/rotate.jpg");
         d->detect(image, kp);
         cv::Mat queryDesc;
         d->compute(image, kp, queryDesc);
+        auto end = std::chrono::system_clock::now();       // 計測終了時刻を保存
+        auto dur = end - start;        // 要した時間を計算
+        auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        std::cout << "get image, and compute: " << msec << " milli sec \n";
 
+        start = std::chrono::system_clock::now();
         cv::Ptr<cv::flann::IndexParams> indexParams = new cv::flann::LshIndexParams(12, 20, 2);
         cv::Ptr<cv::FlannBasedMatcher> m = cv::Ptr<cv::FlannBasedMatcher>(new cv::FlannBasedMatcher(indexParams));
         std::vector<cv::Mat> desc = d9magai::s3utils::getDescriptor(s3client, d9magai::commons::BUCKET, "path/to/matcher1");
+        end = std::chrono::system_clock::now();       // 計測終了時刻を保存
+        dur = end - start;        // 要した時間を計算
+        msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        std::cout << "download descriptor: " << msec << " milli sec \n";
+
+        start = std::chrono::system_clock::now();
         std::vector<cv::DMatch> matches;
         m->add(desc);
+        end = std::chrono::system_clock::now();       // 計測終了時刻を保存
+        dur = end - start;        // 要した時間を計算
+        msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        std::cout << "add desc: " << msec << " milli sec \n";
+        start = std::chrono::system_clock::now();
         m->train();
+        end = std::chrono::system_clock::now();       // 計測終了時刻を保存
+        dur = end - start;        // 要した時間を計算
+        msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        std::cout << "train: " << msec << " milli sec \n";
+        start = std::chrono::system_clock::now();
         m->match(queryDesc, matches);
+        end = std::chrono::system_clock::now();       // 計測終了時刻を保存
+        dur = end - start;        // 要した時間を計算
+        msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        std::cout << "matching: " << msec << " milli sec \n";
 
+
+        start = std::chrono::system_clock::now();
         int votes[s.size()] = { }; // 学習画像の投票箱
         // 投票数の多い画像のIDと特徴点の数を調査
         int maxImageId = -1;
@@ -46,7 +76,10 @@ int main(int argc, char** argv) {
         }
         std::cout << maxImageId << std::endl;
         std::cout << s[maxImageId] << std::endl;
-
+        end = std::chrono::system_clock::now();       // 計測終了時刻を保存
+        dur = end - start;        // 要した時間を計算
+        msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        std::cout << "get once:" << msec << " milli sec \n";
     } catch (std::string str) {
         std::cerr << str << std::endl;
     }
